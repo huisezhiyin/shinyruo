d83e3d69e3bd0c8ff34b13b0a9a5bdd8f8a9234f
Grey <837364695@qq.com>
2018-09-12 13:39:12 +0800

    [build]

--- app_ex/repeater/utils.py
+++ app_ex/repeater/utils.py
@@ -1,20 +1,45 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import GenericViewSet
 import requests
 
 
-def random_number(max_, min_, num):
-    data = {
-        "jsonrpc": "2.0",
-        "method": "generateIntegers",
-        "params": {
-            "apiKey": "ddcdd8bd-09a0-44e9-bb09-cb49010a80ba",
-            "n": num,
-            "min": min_,
-            "max": max_,
-            "replacement": True,
-            "base": 10
-        },
-        "id": 1
-    }
-    r = requests.post(url="https://api.random.org/json-rpc/1/invoke", json=data)
-    return r.json()["result"]["random"]["data"]
+class RandomNumber(object):
+    _instance = None
+    # 这里这个单例模式其实没那么需要
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(RandomNumber, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
 
+    def __init__(self, ):
+        self.apiKey = "ddcdd8bd-09a0-44e9-bb09-cb49010a80ba"
+        self.url = "https://api.random.org/json-rpc/1/invoke"
+
+    def random_number(self, max_: int, num: int = 1, min_: int = 0):
+        data = {
+            "jsonrpc": "2.0",
+            "method": "generateIntegers",
+            "params": {
+                "apiKey": self.apiKey,
+                "n": num,
+                "min": min_,
+                "max": max_,
+                "replacement": True,
+                "base": 10
+            },
+            "id": 1
+        }
+        r = requests.post(url=self.url, json=data)
+        return r.json()["result"]["random"]["data"]
+
+
+class ShinyRuoPageNumberViewSet(GenericViewSet):
+    pagination_class = PageNumberPagination
+
+    def page_response(self, queryset, serializer_class):
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = serializer_class(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = serializer_class(queryset, many=True)
+        return self.get_paginated_response(serializer.data)
--- app_ex/work_ex/models.py
+++ app_ex/work_ex/models.py
@@ -7,8 +7,11 @@ class Lottery(models.Model):
     # 抽奖
     title = models.CharField(max_length=225)
     content = models.TextField()
+    # 发布人
     sponsor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # 是否匿名
     anonymous = models.BooleanField(default=False)
+    # 参与者
     participant = models.ManyToManyField(settings.AUTH_USER_MODEL)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
@@ -29,5 +32,6 @@ class Prize(models.Model):
 class WinningInfo(models.Model):
     lottery = models.ForeignKey(Lottery, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    prize = models.ForeignKey(Prize, on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
