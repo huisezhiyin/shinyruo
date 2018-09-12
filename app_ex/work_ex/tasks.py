5f23782f12c25a6cc0069fc4515bfdf3466864d9
Grey <837364695@qq.com>
2018-09-12 14:48:56 +0800

    [build]

--- app_ex/work_ex/tasks.py
+++ app_ex/work_ex/tasks.py
@@ -3,12 +3,14 @@ from app_ex.work_ex.models import Lottery, Prize, WinningInfo
 from app_ex.user_ex.models import User
 from django.db.models import Sum
 from shinyruo_qaq.celery import app
+from django.conf import settings
 import datetime
 import heapq
 
+random_number = RandomNumber()
 
 @app.task(bind=True)
-def lottery_task(self,lottery_id):
+def lottery_task(self, lottery_id):
     try:
         lottery = Lottery.objects.get(id=lottery_id)
     except Lottery.DoesNotExist:
@@ -18,25 +20,25 @@ def lottery_task(self,lottery_id):
     lottery.ended = True
     lottery.save()
     winner_list = []
-    participant_list = lottery.participant.all()
+    key = f"lottery:{lottery.id}"
+    participant_id_list = settings.LOCAL_REDIS.scard(key)
     # 获取所有奖品数
     prize_queryset = Prize.objects.filter(lottery=lottery)
     prize_number = prize_queryset.aggregate(Sum("number"))["number__sum"]
-    if prize_number <= len(participant_list):
-        random_number = RandomNumber()
-        result_list = random_number.random_number(len(participant_list), prize_number)
+    if prize_number <= len(participant_id_list):
+        result_list = random_number.random_number(len(participant_id_list), prize_number)
         for i in result_list:
-            winner_list.append(participant_list[i])
+            winner_list.append(participant_id_list[i])
     else:
-        winner_list = participant_list
+        winner_list = participant_id_list
     # 按顺序写入奖品 这样做比较粗燥
     # 当winner list无法互相比较的时候是无法压入堆的
     winner_list = heapq.heapify(winner_list)
     for prize in prize_queryset:
         for i in range(prize.number):
-            win = WinningInfo.objects.create(
+            user = User.objects.get(id=heapq.heappop(winner_list))
+            WinningInfo.objects.create(
                 lottery=lottery,
-                user=heapq.heappop(winner_list),
-                prize=prize
-            )
+                user=user,
+                prize=prize)
     return 1
